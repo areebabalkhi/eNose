27fb50d44f679deaf3a2bf93bb515eabf83e9cb5
Areeba <areebabalkhi@gmail.com>
2024-01-09 22:56:13 -0800

    Update eNose.ino to output in csv format.

--- eNose.ino
+++ eNose.ino
@@ -1,9 +1,11 @@
 #include "SparkFun_SGP30_Arduino_Library.h"  // Air quality sensor library
 #include <Wire.h>
 
-const int MQ3Pin = A5;  // Alcohol sensor pin
+const int MQ3Pin = A0;  // Alcohol sensor pin
 SGP30 mySensor;
 long t1, t2;
+
+long CO2, TVOC, H2, ethanol, alcohol;
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,12 +14,15 @@ void setup() {
   Wire.begin();
   Wire.setClock(400000);
 
+  // Serial.println("hello");
+
   if (mySensor.begin() == false) {
     Serial.println(F("SGP30 not detected. Check connections. Freezing..."));
     while (1)
       ;  // Do nothing more
   }
 
+  // Serial.println("hello2");
   mySensor.initAirQuality();
   t1 = millis();
 }
@@ -28,20 +33,23 @@ void loop() {
   if (t2 >= t1 + 1000) {
     t1 = t2;
     mySensor.measureAirQuality();
-    Serial.print("CO2: ");
-    Serial.print(mySensor.CO2);
-    Serial.print(" ppm\tTVOC: ");
-    Serial.print(mySensor.TVOC);
-    Serial.println(" ppb");
-    //get raw values for H2 and Ethanol
-    mySensor.measureRawSignals();
-    Serial.print("Raw H2: ");
-    Serial.print(mySensor.H2);
-    Serial.print(" \tRaw Ethanol: ");
-    Serial.println(mySensor.ethanol);
+    CO2 = mySensor.CO2; // in ppm
+    TVOC = mySensor.TVOC; // in ppb
 
-    float alcRead = analogRead(MQ3Pin);
-    Serial.print("Alc: ");
-    Serial.println(alcRead);
+    mySensor.measureRawSignals();
+    H2 = mySensor.H2;
+    ethanol = mySensor.ethanol;
+
+    alcohol = analogRead(MQ3Pin);
+
+    Serial.print(CO2);
+    Serial.print(", ");
+    Serial.print(TVOC);
+    Serial.print(", ");
+    Serial.print(H2);
+    Serial.print(", ");
+    Serial.print(ethanol);
+    Serial.print(", ");
+    Serial.println(alcohol);
   }
 }
